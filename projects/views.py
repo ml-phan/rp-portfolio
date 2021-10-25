a8177614c714fda815040c7d9594eff70e673360
Phan <phanmanhlinh@yahoo.com>
2021-10-25 12:16:21 +0200

    base.html

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        "projects" : projects
+    }
+    return render(request, "project_index.html", context)
+    
+def project_detail(request):
+    project = Project.objects.get(pk=ak)
+    context = {
+        "projects" : projects
+    }
+    return render(request, "project_detail.html", context)
+    
